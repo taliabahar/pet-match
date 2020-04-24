dbd2f66d15241f13d9cc14e4de4e3be63f684a89
jennifer-s19 <siaojm@yahoo.com>
2020-04-24 12:06:56 -0700

    App.js with router

--- animal-shelters/src/App.js
+++ animal-shelters/src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import HomePage from "./HomePage/HomePage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      
+      <Switch> 
+        <Route path="/quiz"> 
+          <Quiz /> 
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+      </Switch>
 
     </div>
   );
