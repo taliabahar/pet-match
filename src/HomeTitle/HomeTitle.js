20e9ae4d2f0dde8e5ba20694ad1bc7f3e08be5ed
Talia <taliabahar@yahoo.com>
2020-04-30 13:43:17 -0700

    styled home page and quiz

--- src/HomeTitle/HomeTitle.js
+++ src/HomeTitle/HomeTitle.js
@@ -2,16 +2,90 @@ import "./HomeTitle.css";
 
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
+import dog from "../images/dog.png";
+import rabbit from "../images/rabbit.png";
+import bird from "../images/bird.png";
 
 export default function HomeTitle() {
   return (
     <div className="title">
-      {/* <Navbar bg="light">
-        <Navbar.Brand id="test" href="/">
-          Pet Match
-        </Navbar.Brand>
-      </Navbar> */}
-      <h1>hola</h1>
+      <h1>Welcome to Pet match!</h1>
+      <h2>Let's find the right pet for you</h2>
+      <h3>
+        Our Mission <img className="animalPic" src={dog}></img>
+      </h3>
+      <p id="mission">
+        Pet match advocates for the adoption of shelter animals and
+        human-companion animal bonds. We provide companion animal suggestions
+        based upon your lifestyle and theirs. We make the process of finding
+        one's perfect animal as easy as possible. Take our Quiz to find your new
+        best friend!
+      </p>
+      <div id="facts">
+        <h3 className="info">
+          Shelter Facts <img className="animalPic" src={rabbit}></img>
+        </h3>
+        <ul>
+          <li>Each year, about 3.7 million animals are euthanized </li>
+          <li>
+            5 in 10 dogs and 7 in 10 cats in shelters are euthanized simply
+            because there is no one to adopt them
+          </li>
+          <li>25 percent of shelter dogs are purebred </li>
+          <li>
+            Only 10 to 20 percent of dogs and cats owned are adopted from
+            shelters
+          </li>
+          <li>
+            Approximately $7.6 million companion animals enter animal shelters
+            nationwide every year
+          </li>
+          <li>The average age of animals entering shelters is 18 months</li>
+          <li>
+            It costs taxpayers $2 billion annually to round up, house, kill and
+            dispose of homeless animals
+          </li>
+        </ul>
+      </div>
+      <div id="reasons">
+        <h3 className="info">
+          Reasons to Adopt From a Shelter{" "}
+          <img className="animalPic" src={bird}></img>
+        </h3>
+        <ul>
+          <li>
+            The financial cost of adopting a shelter animal is almost always
+            less than purchasing an animal from a breeder.
+          </li>
+          <li>
+            Shelter animals are given vaccinations upon arrival and go through a
+            behavior screening process{" "}
+          </li>
+          <li>
+            Overburdened shelters take in millions of stray, abused, and lost
+            animals every year, and by adopting an animal, you’re making room
+            for others.
+          </li>
+          <li>
+            Owning a pet has psychological, emotional and physical benefits
+          </li>
+          <li>
+            {" "}
+            Most shelter pets wound up there because of a human problem like a
+            move or a divorce, not because the animals did anything wrong. Many
+            are already housetrained and used to living with families
+          </li>
+          <li>
+            The average cost of basic food, supplies, medical care and training
+            for a dog or cat is $400 to $700 annually
+          </li>
+          <li>
+            {" "}
+            By adopting you won’t be supporting the cruel practice of puppy
+            mills where dogs are overbred and spend most of their lives in cages
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { getToken, simpleFetch } from "./api";
 import Gallery from "./Gallery";
+import "./Quiz.css";
 
 // import Gallery -> pass it quizResults in props
 
@@ -48,143 +49,151 @@ function Quiz() {
     <div className="Quiz">
       <h1 id="QuizHeader">Let's Find Your Match</h1>
       <form>
-        <p>What kind of animal are you looking for?</p>
-        <input
-          placeholder="Dog, Cat, Rabbit, Bird, etc."
-          type="text"
-          onChange={(e) => setAnimalType(e.target.value)}
-          value={animalType}
-        ></input>
-
-        <p>What is your postal code?</p>
-        <input
-          placeholder="Ex.90045"
-          type="text"
-          onChange={(e) => setPostcode(e.target.value)}
-          value={postcode}
-        ></input>
-
-        <p>Do you have a desired Age?</p>
-        <input
-          type="radio"
-          id="baby"
-          name="age"
-          value="baby"
-          onChange={(e) => setAge(e.target.value)}
-        ></input>
-        <label htmlFor="baby">Baby</label>
-        <input
-          type="radio"
-          id="young"
-          name="age"
-          value="young"
-          onChange={(e) => setAge(e.target.value)}
-        ></input>
-        <label htmlFor="large">Young</label>
-        <input
-          type="radio"
-          id="adult"
-          name="age"
-          value="adult"
-          onChange={(e) => setAge(e.target.value)}
-        ></input>
-        <label htmlFor="adult">Adult</label>
-        <input
-          type="radio"
-          id="senior"
-          name="age"
-          value="senior"
-          onChange={(e) => setAge(e.target.value)}
-        ></input>
-        <label htmlFor="senior">Senior</label>
-        <p>What size animal are you looking for?</p>
-        <input
-          type="radio"
-          id="small"
-          name="size"
-          value="small"
-          onChange={(e) => setSize(e.target.value)}
-        ></input>
-        <label htmlFor="small">Small</label>
-        <input
-          type="radio"
-          id="medium"
-          name="size"
-          value="medium"
-          onChange={(e) => setSize(e.target.value)}
-        ></input>
-        <label htmlFor="medium">Medium</label>
-        <input
-          type="radio"
-          id="large"
-          name="size"
-          value="large"
-          onChange={(e) => setSize(e.target.value)}
-        ></input>
-        <label htmlFor="large">Large</label>
-        <input
-          type="radio"
-          id="xlarge"
-          name="size"
-          value="xlarge"
-          onChange={(e) => setSize(e.target.value)}
-        ></input>
-        <label htmlFor="xlarge">X-Large</label>
-
-        <p>Do you have kids?</p>
-        <input
-          type="radio"
-          id="yes"
-          name="kids"
-          value="true"
-          onChange={(e) => setHasKids(e.target.value)}
-        ></input>
-        <label htmlFor="yes">Yes</label>
-        <input
-          type="radio"
-          id="no"
-          name="kids"
-          value="false"
-          onChange={(e) => setHasKids(e.target.value)}
-        ></input>
-        <label htmlFor="no">No</label>
-
-        <p>Do you have dogs?</p>
-        <input
-          type="radio"
-          id="yes"
-          name="dogs"
-          value="true"
-          onChange={(e) => setHasDogs(e.target.value)}
-        ></input>
-        <label htmlFor="yes">Yes</label>
-        <input
-          type="radio"
-          id="no"
-          name="dogs"
-          value="false"
-          onChange={(e) => setHasDogs(e.target.value)}
-        ></input>
-        <label htmlFor="no">No</label>
-
-        <p>Do you have cats?</p>
-        <input
-          type="radio"
-          id="yes"
-          name="cats"
-          value="true"
-          onChange={(e) => setHasCats(e.target.value)}
-        ></input>
-        <label htmlFor="yes">Yes</label>
-        <input
-          type="radio"
-          id="no"
-          name="cats"
-          value="false"
-          onChange={(e) => setHasCats(e.target.value)}
-        ></input>
-        <label htmlFor="no">No</label>
-
+        <div className="quizQ">
+          <p>What kind of animal are you looking for?</p>
+          <input
+            placeholder="Ex.Dog, Cat, Rabbit, Bird, etc."
+            type="text"
+            onChange={(e) => setAnimalType(e.target.value)}
+            value={animalType}
+          ></input>
+        </div>
+        <div className="quizQ">
+          <p>What is your postal code?</p>
+          <input
+            placeholder="Ex.90045"
+            type="text"
+            onChange={(e) => setPostcode(e.target.value)}
+            value={postcode}
+          ></input>
+        </div>
+        <div className="quizQ">
+          <p>Do you have a desired Age?</p>
+          <input
+            type="radio"
+            id="baby"
+            name="age"
+            value="baby"
+            onChange={(e) => setAge(e.target.value)}
+          ></input>
+          <label htmlFor="baby">Baby</label>
+          <input
+            type="radio"
+            id="young"
+            name="age"
+            value="young"
+            onChange={(e) => setAge(e.target.value)}
+          ></input>
+          <label htmlFor="large">Young</label>
+          <input
+            type="radio"
+            id="adult"
+            name="age"
+            value="adult"
+            onChange={(e) => setAge(e.target.value)}
+          ></input>
+          <label htmlFor="adult">Adult</label>
+          <input
+            type="radio"
+            id="senior"
+            name="age"
+            value="senior"
+            onChange={(e) => setAge(e.target.value)}
+          ></input>
+          <label htmlFor="senior">Senior</label>
+        </div>
+        <div className="quizQ">
+          <p>What size animal are you looking for?</p>
+          <input
+            type="radio"
+            id="small"
+            name="size"
+            value="small"
+            onChange={(e) => setSize(e.target.value)}
+          ></input>
+          <label htmlFor="small">Small</label>
+          <input
+            type="radio"
+            id="medium"
+            name="size"
+            value="medium"
+            onChange={(e) => setSize(e.target.value)}
+          ></input>
+          <label htmlFor="medium">Medium</label>
+          <input
+            type="radio"
+            id="large"
+            name="size"
+            value="large"
+            onChange={(e) => setSize(e.target.value)}
+          ></input>
+          <label htmlFor="large">Large</label>
+          <input
+            type="radio"
+            id="xlarge"
+            name="size"
+            value="xlarge"
+            onChange={(e) => setSize(e.target.value)}
+          ></input>
+          <label htmlFor="xlarge">X-Large</label>
+        </div>
+        <div className="quizQ">
+          <p>Do you have kids?</p>
+          <input
+            type="radio"
+            id="yes"
+            name="kids"
+            value="true"
+            onChange={(e) => setHasKids(e.target.value)}
+          ></input>
+          <label htmlFor="yes">Yes</label>
+          <input
+            type="radio"
+            id="no"
+            name="kids"
+            value="false"
+            onChange={(e) => setHasKids(e.target.value)}
+          ></input>
+          <label htmlFor="no">No</label>
+        </div>
+        <div className="quizQ">
+          <p>Do you have dogs?</p>
+          <input
+            type="radio"
+            id="yes"
+            name="dogs"
+            value="true"
+            onChange={(e) => setHasDogs(e.target.value)}
+          ></input>
+          <label htmlFor="yes">Yes</label>
+          <input
+            type="radio"
+            id="no"
+            name="dogs"
+            value="false"
+            onChange={(e) => setHasDogs(e.target.value)}
+          ></input>
+          <label htmlFor="no">No</label>
+        </div>
+        <div className="quizQ">
+          <p>Do you have cats?</p>
+          <input
+            type="radio"
+            id="yes"
+            name="cats"
+            value="true"
+            onChange={(e) => setHasCats(e.target.value)}
+          ></input>
+          <label htmlFor="yes">Yes</label>
+          <input
+            type="radio"
+            id="no"
+            name="cats"
+            value="false"
+            onChange={(e) => setHasCats(e.target.value)}
+          ></input>
+          <label htmlFor="no">No</label>
+        </div>
         {/* <p>Are you an experienced adopter?</p>
         <input
           type="radio"
