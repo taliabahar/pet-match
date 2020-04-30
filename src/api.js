5c9804ffad3b7ab6d8fee39bd6fabbc71afb24b0
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-30 01:48:12 -0700

    Added use of env files for api key and secret

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
-const authUrl = 'https://api.petfinder.com/v2/oauth2/token';
-const serviceUrl = 'https://api.petfinder.com/v2';
+const authUrl = "https://api.petfinder.com/v2/oauth2/token";
+const serviceUrl = "https://api.petfinder.com/v2";
 
-// Ask Marco or Masao about keeping these safe
-const apiKey = '';
-const secret = '';
+const apiKey = process.env.REACT_APP_API_KEY.replace(/['";]+/g, "");
+const secret = process.env.REACT_APP_SECRET.replace(/['";]+/g, "");
 
 // Okay to cache the token and base URL in memory. Some people like to put it
 // in local storage but that can in theory be compromised if the is an XSS
@@ -14,33 +13,34 @@ let token = undefined;
 // to get you a token, and it puts the token in memory.
 export async function getToken() {
   const config = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    mode: 'cors',
+    mode: "cors",
     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
   };
   fetch(authUrl, config)
-    .then(response => {
+    .then((response) => {
       return response.json();
     })
-    .then(response => {
+    .then((response) => {
       token = response.access_token;
     });
 }
 
 // Returns a promise, so add a then/catch chain when you call this
 export async function simpleFetch(path, qs = {}) {
-  const config = { method: 'GET', headers: new Headers(), mode: 'cors' };
-  config.headers.append('Authorization', `Bearer ${token}`);
+  const config = { method: "GET", headers: new Headers(), mode: "cors" };
+  config.headers.append("Authorization", `Bearer ${token}`);
   console.log(qs);
   let query = Object.entries(qs || {})
     .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
-    .join('&');
-  query = query ? `?${query}` : '';
+    .join("&");
+  query = query ? `?${query}` : "";
   console.log(query);
-  return fetch(`${serviceUrl}/${path}${query}`, config).then(response => {
+  // console.log(`${serviceUrl}/${path}${query}`);
+  return fetch(`${serviceUrl}/${path}${query}`, config).then((response) => {
     if (response.ok) {
       return response.json();
     } else if (response.status === 401) {
