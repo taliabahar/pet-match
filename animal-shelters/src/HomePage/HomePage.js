f3b60719dc8fceb8445450c743a3cf32979c8ffa
Talia <taliabahar@yahoo.com>
2020-04-30 00:29:16 -0700

    started styling homepage and created Quiz.js and Quiz form

--- animal-shelters/src/HomePage/HomePage.js
+++ animal-shelters/src/HomePage/HomePage.js
@@ -3,29 +3,30 @@ import "./HomePage.css";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Gallery from "../Gallery";
 import { Button } from "react-bootstrap";
-//import Quiz from '../Quiz';
+import Quiz from "../Quiz";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function HomePage() {
   return (
     <div>
-      <div className="body">
+      <div className="homeBody">
         <Router>
           <div>
             <Link to="/quiz">
-              <Button color="white" className="is-rounded">
+              <Button id="quiz" color="white" className="is-rounded">
                 <span>Take Quiz</span>
               </Button>
             </Link>
             <br />
             <br />
             <Link to="/gallery">
-              <Button color="white" className="is-rounded">
+              <Button id="gallery" color="white" className="is-rounded">
                 <span>View Gallery</span>
               </Button>
             </Link>
             <Switch>
               <Route path="/Gallery" component={Gallery} />
-              {/* <Route path="/Quiz" component={Quiz} />  */}
+              <Route path="/Quiz" component={Quiz} />
             </Switch>
           </div>
         </Router>
