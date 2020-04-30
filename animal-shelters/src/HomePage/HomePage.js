85babef1edf81665a3d0cc8c1c20a4b91ef1f0e2
Talia <taliabahar@yahoo.com>
2020-04-29 22:23:09 -0700

    fixed comment issue

--- animal-shelters/src/HomePage/HomePage.js
+++ animal-shelters/src/HomePage/HomePage.js
@@ -1,17 +1,14 @@
-
 import React from "react";
 import "./HomePage.css";
-import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
-import Gallery from '../Gallery';
-import { Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Gallery from "../Gallery";
+import { Button } from "react-bootstrap";
 //import Quiz from '../Quiz';
 
-
-
 export default function HomePage() {
-    return (
+  return (
     <div>
-       <div className="body">
+      <div className="body">
         <Router>
           <div>
             <Link to="/quiz">
@@ -19,20 +16,20 @@ export default function HomePage() {
                 <span>Take Quiz</span>
               </Button>
             </Link>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <Link to="/gallery">
               <Button color="white" className="is-rounded">
                 <span>View Gallery</span>
               </Button>
             </Link>
-          <Switch>
-            <Route path="/Gallery" component={Gallery} />
-//             <Route path="/Quiz" component={Quiz} /> 
-          </Switch>
+            <Switch>
+              <Route path="/Gallery" component={Gallery} />
+              {/* <Route path="/Quiz" component={Quiz} />  */}
+            </Switch>
           </div>
         </Router>
-      </div> 
+      </div>
     </div>
   );
 }
