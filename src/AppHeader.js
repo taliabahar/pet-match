aab5f599897f55356b9befbb7052ceeac946b3be
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-30 17:41:49 -0700

    Update css

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -3,37 +3,32 @@ import Navbar from "react-bootstrap/NavBar";
 import Nav from "react-bootstrap/Nav";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Gallery from "./Gallery";
-// import { Button } from "react-bootstrap";
 import Quiz from "./Quiz";
-import HomeTitle from "./HomeTitle/HomeTitle";
-// import Link from "react-bootstrap/Nav";
-import magnifyingGlass from "./search.png";
+import Home from "./Home/Home";
+import magnifyingGlass from "./images/search.png";
 
 import "./AppHeader.css";
-// import resume from "./resume.pdf";
 
 export default function AppHeader() {
   return (
     <Router>
-      {/* <div className="header-navbar"> */}
       <Navbar>
         <Link to="/" className="header-logo">
           Pet Match <img src={magnifyingGlass}></img>
         </Link>
         <Nav className="header-links">
-          <Link to="/gallery">Gallery</Link>
+          {/* <Link to="/gallery">Gallery</Link> */}
           <Link to="/quiz">Quiz</Link>
         </Nav>
       </Navbar>
-      {/* </div> */}
       <div>
         <Switch>
           <Route exact path="/">
-            <HomeTitle />
+            <Home />
           </Route>
-          <Route path="/gallery">
+          {/* <Route path="/gallery">
             <Gallery />
-          </Route>
+          </Route> */}
           <Route path="/quiz">
             <Quiz />
           </Route>
